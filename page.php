8ea2484d8c4b081d8f3ff3db8536e533b1b5ffb4
Kuganeshan Nair <kuganeshan.nair@gmail.com>
2019-02-18 10:28:52 +0800

    web design update

---
+++ page.php
@@ -0,0 +1,158 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<title>Car Rental Portal | Page details</title>
+<!--Bootstrap -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+<!--Custome Style -->
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<!--OWL Carousel slider-->
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+<!--slick-slider -->
+<link href="assets/css/slick.css" rel="stylesheet">
+<!--bootstrap-slider -->
+<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+<!--FontAwesome Font Style -->
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- SWITCHER -->
+		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/red.css" title="red" media="all" data-default-color="true" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/orange.css" title="orange" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/blue.css" title="blue" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/pink.css" title="pink" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/green.css" title="green" media="all" />
+		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/purple.css" title="purple" media="all" />
+        
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+</head>
+<body>
+<<!-- Start Switcher -->
+<?php include('includes/colorswitcher.php');?>
+<!-- /Switcher -->  
+        
+<!--Header-->
+<?php include('includes/header.php');?>
+                     
+<div id="body">
+ <div class="container">
+ <div class="section-header text-center">
+ <br><br>
+		<h1><p>MIAA offers the most complete and user-friendly rental software on the web.<br>
+
+         In short you will be able to manage all your bookings, your fleet maintenance and your customers.<br>
+
+         And with our valuable management dashboards, you will finally be able to monitor and run your company based on numbers.<br>
+	   </p>
+	     <left><h2>Vision: </h2>
+		</p>
+		<p>To lead the market supported by a valued structure that guarantees the expected utilities and the total satisfaction 
+		of clients and collaborators.</p>
+		<h2>Mision: </h2>
+		</p>
+		<p>To provide with excellence rent transportation solutions, that stands far from the expectations of our clients, partners and collaborators.</p>
+		</div></h1>
+ <div id="content">
+    <table>
+	<tr>
+	<th><h2>Contact Us</h2></th>
+	<th><h2>Opening Hours</h2></th>
+	
+	</tr>
+	<td><h4>
+	MIAA Car Rental Enterprise<br>
+	78, Jalan Tengah,<br>
+	Bayan Baru, Bayan Lepas, <br>
+	11900 Bayan Lepas, <br>
+	Pulau Pinang
+	<br><br><br>
+	Tel : +604 432 1212
+	<br>
+	Fax : +604 121 2234
+	<br>
+	Mobile : +60124339840
+	<br>
+	(WhatsApp & WeChat)
+	<br>
+	Email : miaacarrental@yahoo.com
+	</h4></td>
+	
+	<td><h4>
+	Monday - Friday:
+	<br>
+	9:00am - 9:00pm
+	<br><br>
+	Saturday:
+	<br>
+	9:00am - 5:00pm 
+	<br><br>
+	Sunday:
+	<br>
+	10:00 - 3:00pm
+	<br>
+	Closed on Public Holidays
+	</h4></td>
+	
+	</table>
+    </div>
+</div>
+
+
+
+
+
+<<!--Footer -->
+<?php include('includes/footer.php');?>
+<!-- /Footer--> 
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+<!--/Back to top--> 
+
+<!--Login-Form -->
+<?php include('includes/login.php');?>
+<!--/Login-Form --> 
+
+<!--Register-Form -->
+<?php include('includes/registration.php');?>
+
+<!--/Register-Form --> 
+
+<!--Forgot-password-Form -->
+<?php include('includes/forgotpassword.php');?>
+<!--/Forgot-password-Form --> 
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+<!-- Mirrored from themes.webmasterdriver.net/carforyou/demo/about-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Jun 2017 07:26:12 GMT -->
+</html>
